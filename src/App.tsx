eafca94e049950c5a076c0c1819663942f757dee
devmhs <mhs.02@hotmail.com>
2025-01-03 14:12:51 -0300

    Adicionada foto e botão para baixar currículo

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,34 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Header from './components/Header';
-import { Hero } from './components/Hero';
 import './styles/global.css';
 
 const App = () => {
-    return (
-        <div>
-           
-            <Header />
-            <section id="home">
-                <Home />
-            </section>
-            <section id="about">
-                <About />
-            </section>
-            <section id="projects">
-                <Projects />
-            </section>
-            <section id="contact">
-                <Contact />
-            </section>
+  
 
-            {/* Botão fixo para baixar o CV */}
-            <div className="fixed-cv-button">
-                <a href="./Marcos Henrique - Dev..pdf" download="Marcos Henrique - Dev..pdf">
-                    Baixar CV
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <section id="home">
+        <Home />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+
+      <div className="fixed-cv-button">
+        <a href="./Marcos Henrique - Dev..pdf" download="Marcos Henrique - Dev..pdf">
+          Baixar CV
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './About.css';
+import minhaFoto from '../assets/foto-perfil.jpeg';
 
 const About = () => {
     return (
         <section className="about">
-            <h2>Sobre Mim</h2>
-            <p>
-                Sou um desenvolvedor apaixonado por criar soluções criativas e eficientes. Tenho experiência em
-                tecnologias como React, Node.js, e TypeScript.
-            </p>
+            <div className="about-content">
+                <img src={minhaFoto} alt="Foto de Marcos Henrique" className="about-image" />
+                <div className="about-text">
+                    <h2>Sobre Mim</h2>
+                    <p>
+                        Sou um desenvolvedor apaixonado por criar soluções criativas e eficientes. Tenho experiência em
+                        tecnologias como React, Node.js, e TypeScript.
+                    </p>
+                    <div className="download-cv">
+    <a href="./Marcos_Henrique_Curriculo.pdf" download="Marcos_Henrique_Curriculo.pdf">
+        Saiba mais sobre mim 
+    </a>
+</div>
+
+                </div>
+            </div>
         </section>
     );
 };
